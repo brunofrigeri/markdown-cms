ec323316eba6cba1819c0b5e8fe20784f4a40e44
Bruno Frigeri <bpiraja97@gmail.com>
2023-01-05 18:10:39 -0300

    feat: cleanup index route creating a new hook named onMouseMove

---
+++ hooks/useMouseMove.ts
@@ -0,0 +1,83 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export const useMouseMove = () => {
+  const [isDraggingLeft, setIsDraggingLeft] = useState<boolean>(false)
+  const [isDraggingRight, setIsDraggingRight] = useState<boolean>(false)
+
+  const onMouseDown = (event: MouseEvent) => {
+    if ((event.target as HTMLElement).id === 'handlerMarkdownAndPreview') {
+      event.preventDefault()
+      setIsDraggingRight(true)
+    } else if ((event.target as HTMLElement).id === 'handlerFilesNMarkdown') {
+      event.preventDefault()
+      setIsDraggingLeft(true)
+    }
+  }
+
+  const onMouseMove = useCallback(
+    (event: MouseEvent) => {
+      if (isDraggingLeft) {
+        event.preventDefault()
+        const handlerFilesNMarkdown = document.querySelector(
+          '#handlerFilesNMarkdown'
+        )
+        const container = handlerFilesNMarkdown?.parentElement
+        const filesElement = container?.querySelector('#files') as HTMLElement
+
+        const minWidth = 200
+        const maxWidth = window.innerWidth / 2
+
+        if (filesElement) {
+          filesElement.style.width =
+            (event.clientX > maxWidth
+              ? maxWidth
+              : Math.max(event.clientX, minWidth)) + 'px'
+          filesElement.style.flexGrow = '0'
+        }
+      } else if (isDraggingRight) {
+        event.preventDefault()
+        const handlerFilesNMarkdown = document.querySelector(
+          '#handlerMarkdownAndPreview'
+        )
+
+        const container = handlerFilesNMarkdown?.parentElement
+        const previewElement = container?.querySelector(
+          '#preview'
+        ) as HTMLElement
+
+        const minWidth = 300
+        // For just two columns uses 1.5, for three, uses 2
+        const maxWidth = window.innerWidth / 1.5
+
+        const clientX = (container?.clientWidth ?? 0) - event.clientX
+
+        if (previewElement) {
+          previewElement.style.flex = '1 1 auto'
+          previewElement.style.width =
+            (clientX > maxWidth ? maxWidth : Math.max(clientX, minWidth)) + 'px'
+          previewElement.style.flexGrow = '0'
+        }
+      } else {
+        return false
+      }
+    },
+    [isDraggingLeft, isDraggingRight]
+  )
+
+  const onMouseUp = () => {
+    setIsDraggingLeft(false)
+    setIsDraggingRight(false)
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', onMouseDown)
+    document.addEventListener('mousemove', onMouseMove)
+    document.addEventListener('mouseup', onMouseUp)
+
+    return () => {
+      document.removeEventListener('mousedown', onMouseDown)
+      document.removeEventListener('mousemove', onMouseMove)
+      document.removeEventListener('mouseup', onMouseUp)
+    }
+  }, [onMouseMove])
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,106 +1,20 @@
-import { GetServerSideProps } from 'next'
-import React, { useCallback, useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import React, { useState } from 'react'
 // import FileManager from '../components/FileManager'
 import MDXTextArea from '../components/MDXTextArea'
 import PreviewContent from '../components/PreviewContent'
+import { useMouseMove } from '../hooks/useMouseMove'
 import styles from '../styles/Mdx.module.css'
-import { compileRemarkOnSSR } from '../lib/remarkSSR'
 
-interface MDXEditorProps {
-  content: string
-  isParsed?: boolean
-}
+interface MDXEditorProps {}
 
-const MDXEditor: React.FC<MDXEditorProps> = ({ content, isParsed = false }) => {
-  const router = useRouter()
-  const [isDraggingLeft, setIsDraggingLeft] = useState<boolean>(false)
-  const [isDraggingRight, setIsDraggingRight] = useState<boolean>(false)
-
+const MDXEditor: React.FC<MDXEditorProps> = () => {
   const [currentContent, setCurrentContent] = useState<string>('')
 
-  useEffect(() => {
-    if (content) {
-      setCurrentContent(content)
-    }
-  }, [content])
+  useMouseMove()
 
   // const previewMarkdownWithSyntaxHighlight = () => {
   //   router.replace(`${router.asPath}?content=${currentContent}&isParsed=true`)
   // }
-
-  const onMouseDown = (event: MouseEvent) => {
-    if ((event.target as HTMLElement).id === 'handlerMarkdownAndPreview') {
-      setIsDraggingRight(true)
-    } else if ((event.target as HTMLElement).id === 'handlerFilesNMarkdown') {
-      setIsDraggingLeft(true)
-    }
-  }
-
-  const onMouseMove = useCallback(
-    (event: MouseEvent) => {
-      if (isDraggingLeft) {
-        const handlerFilesNMarkdown = document.querySelector(
-          '#handlerFilesNMarkdown'
-        )
-        const container = handlerFilesNMarkdown?.parentElement
-        const filesElement = container?.querySelector('#files') as HTMLElement
-
-        const minWidth = 200
-        const maxWidth = window.innerWidth / 2
-
-        if (filesElement) {
-          filesElement.style.width =
-            (event.clientX > maxWidth
-              ? maxWidth
-              : Math.max(event.clientX, minWidth)) + 'px'
-          filesElement.style.flexGrow = '0'
-        }
-      } else if (isDraggingRight) {
-        const handlerFilesNMarkdown = document.querySelector(
-          '#handlerMarkdownAndPreview'
-        )
-
-        const container = handlerFilesNMarkdown?.parentElement
-        const previewElement = container?.querySelector(
-          '#preview'
-        ) as HTMLElement
-
-        const minWidth = 300
-        // For just two columns uses 1.5, for three, uses 2
-        const maxWidth = window.innerWidth / 1.5
-
-        const clientX = (container?.clientWidth ?? 0) - event.clientX
-
-        if (previewElement) {
-          previewElement.style.flex = '1 1 auto'
-          previewElement.style.width =
-            (clientX > maxWidth ? maxWidth : Math.max(clientX, minWidth)) + 'px'
-          previewElement.style.flexGrow = '0'
-        }
-      } else {
-        return false
-      }
-    },
-    [isDraggingLeft, isDraggingRight]
-  )
-
-  const onMouseUp = () => {
-    setIsDraggingLeft(false)
-    setIsDraggingRight(false)
-  }
-
-  useEffect(() => {
-    document.addEventListener('mousedown', onMouseDown)
-    document.addEventListener('mousemove', onMouseMove)
-    document.addEventListener('mouseup', onMouseUp)
-
-    return () => {
-      document.removeEventListener('mousedown', onMouseDown)
-      document.removeEventListener('mousemove', onMouseMove)
-      document.removeEventListener('mouseup', onMouseUp)
-    }
-  }, [onMouseMove])
 
   return (
     <div id="container" className={styles.container}>
@@ -112,17 +26,5 @@ const MDXEditor: React.FC<MDXEditorProps> = ({ content, isParsed = false }) => {
     </div>
   )
 }
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const { content } = context.query
-
-//   const source = await compileRemarkOnSSR(content as string)
-
-//   return {
-//     props: {
-//       content: source.toString(),
-//     },
-//   }
-// }
 
 export default MDXEditor
